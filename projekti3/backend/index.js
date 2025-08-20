b0053baa7b6a4aeaa6eb9ec835250672371c9f2e
Sanni Penttilä <sanni.adine@gmail.com>
2025-08-20 17:37:39 +0300

    jokos se nyt olis yhdistetty

--- projekti3/backend/index.js
+++ projekti3/backend/index.js
@@ -4,102 +4,104 @@ const morgan = require('morgan');
 const cors = require('cors');
 const path = require('path');
 
-
-app.use(express.json())
+app.use(express.json());
 app.use(morgan('tiny'));
-app.use(cors())
-app.use(express.static(path.join(__dirname, 'frontend_build')));
-
+app.use(cors());
 
 let persons = [
     {
-  "persons": [
-    {
-      "name": "Arto Hellas",
-      "number": "040-123456",
-      "id": "1"
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "39-44-5323523",
-      "id": "2"
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": "3"
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": "4"
+        "persons": [
+            {
+                "name": "Arto Hellas",
+                "number": "040-123456",
+                "id": "1"
+            },
+            {
+                "name": "Ada Lovelace",
+                "number": "39-44-5323523",
+                "id": "2"
+            },
+            {
+                "name": "Dan Abramov",
+                "number": "12-43-234345",
+                "id": "3"
+            },
+            {
+                "name": "Mary Poppendieck",
+                "number": "39-23-6423122",
+                "id": "4"
+            }
+        ]
     }
-  ]
-    }
-];  
+];
 
+app.use(express.static(path.join(__dirname, '../frontend/dist')));
 
-app.get( '/', (request, response) => {
+app.get('/', (request, response) => {
     response.send('<h1>Welcome to the Phonebook API</h1>');
-    });
+});
 
 app.get('/api/persons', (request, response) => {
-  response.json(persons[0].persons);
+    response.json(persons[0].persons);
 });
 
 app.get('/info', (request, response) => {
-  const personMany = persons[0].persons.length;
-  const timeNow = new Date().toString();
-  response.send(`<p>Phonebook has info for ${personMany} people</p><p>${timeNow}</p>`);
+    const personMany = persons[0].persons.length;
+    const timeNow = new Date().toString();
+    response.send(`<p>Phonebook has info for ${personMany} people</p><p>${timeNow}</p>`);
 });
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const plist = persons[0].persons;
-  const person = plist.find(p => p.id === id);
+    const id = request.params.id.toString();
+    const plist = persons[0].persons;
+    const person = plist.find(p => p.id === id);
 
-  if (person) {
-    response.json(person);
-  } else {
-    response.status(404).json({ error: 'Person not found' });
-  }
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).json({ error: 'Person not found' });
+    }
 });
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id;
-  const pList = persons[0].persons;
-  persons[0].persons = pList.filter(p => p.id !== id);
-  response.status(204).end();
+    const id = request.params.id;
+    const pList = persons[0].persons;
+    persons[0].persons = pList.filter(p => p.id !== id);
+    response.status(204).end();
 });
 
 app.post('/api/persons', (request, response) => {
-  const { name, number} = request.body;
+    const { name, number } = request.body;
     if (!name || !number) {
-        return response.status(400).json({ 
-            error: 'Name or number is missing' });
+        return response.status(400).json({
+            error: 'Name or number is missing'
+        });
     }
-   
+
     const pList = persons[0].persons;
     const notNewPerson = pList.find(p => p.name === name);
     if (notNewPerson) {
-        return response.status(400).json({ 
-            error: 'Name must be unique' });
-    }  
+        return response.status(400).json({
+            error: 'Name must be unique'
+        });
+    }
 
-  const addPerson = {
-    name,
-    number,
-    id: (Math.random() * 1000).toFixed(0),
-  }
-  console.log(addPerson)
-  response.json(addPerson);
+    const addPerson = {
+        name,
+        number,
+        id: (Math.random() * 1000).toFixed(0),
+    };
+
+    persons[0].persons.push(addPerson);
+    response.json(addPerson);
 });
 
 app.get('*', (request, response) => {
-  responses.sendFile(Path2D.join(__dirname, 'frontend_build', 'index.html'));
+    response.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}/`);
-}); 
+    console.log(`Server running on port ${PORT}/`);
+});
+
